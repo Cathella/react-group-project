b4cb468c53fbfea477ad72e1eb3cd549c7fc55e8
Cathella <ncathie214@gmail.com>
2021-12-21 20:08:23 +0300

    Add image logo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import logo from './planet.png';
 
 const Nav = () => (
   <header>
     <nav>
-      <Link to="/" className="logo">Space X</Link>
+      <Link to="/" className="logo">
+        <img src={logo} alt="Logo" />
+        <span>Space Travelers Hub</span>
+      </Link>
     </nav>
   </header>
 );
