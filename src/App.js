b99b6287f8629cc0ea53dd3009ac169b047615bb
Carlos <carlos.anezeh@gmail.com>
2021-12-21 11:34:54 -0600

    Fix linter issues.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
