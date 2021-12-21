a286b013c7afa8c46236e82a471373956d21c4f8
Cathella <ncathie214@gmail.com>
2021-12-21 19:52:12 +0300

    Add Nav component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="App" />
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+  <header>
+    <nav>
+      <Link to="/" className="logo">Space X</Link>
+    </nav>
+  </header>
+);
+
+export default Nav;
