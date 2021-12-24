1c9ec838223b8a60aa5caed80c62bb513912af9e
Cathella <ncathie214@gmail.com>
2021-12-24 03:10:23 +0300

    Add leave mission action

--- src/components/Missions/Mission.js
+++ src/components/Missions/Mission.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../../redux/missions/missions';
 
-const Mission = ({ name, description }) => (
-  <tr>
-    <th className="mission-name">{name}</th>
-    <td className="description">{description}</td>
-    <td><span className="badge">Not a Member</span></td>
-    <td><button type="button">Join Mission</button></td>
-  </tr>
-);
+const Mission = (props) => {
+  const dispatch = useDispatch();
+  const { data } = props;
+  const { description, status } = data;
+  const id = data.mission_id;
+  const name = data.mission_name;
+
+  const joinButton = () => {
+    dispatch(joinMission(id));
+  };
+
+  const changeStatus = () => {
+    if (status === true) {
+      return null;
+    }
+    return joinButton();
+  };
+
+  return (
+    <tr>
+      <td className="mission-name">
+        <b>{name}</b>
+      </td>
+      <td className="description">{description}</td>
+      <td>
+        <span className="badge">Not a Member</span>
+      </td>
+      <td><button type="button">Join Mission</button></td>
+    </tr>
+  );
+};
 
 Mission.propTypes = {
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    mission_id: PropTypes.string.isRequired,
+    mission_name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    status: PropTypes.bool,
+  }).isRequired,
 };
 
 export default Mission;
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,10 +1,16 @@
 const GET_MISSIONS = 'missionStore/missions/GET_MISSIONS';
 const JOIN_MISSION = 'missionStore/missions/JOIN_MISSION';
+const LEAVE_MISSION = 'missionStore/missions/LEAVE_MISSION';
 
 const initialState = [];
 
 export const joinMission = (payload) => ({
   type: JOIN_MISSION,
+  payload,
+});
+
+export const leaveMission = (payload) => ({
+  type: LEAVE_MISSION,
   payload,
 });
 
@@ -30,6 +36,13 @@ const missionsReducer = (state = initialState, action) => {
           return mission;
         }
         return { ...mission, status: true };
+      });
+    case LEAVE_MISSION:
+      return state.map((mission) => {
+        if (mission.mission_id === action.payload) {
+          return { ...mission, status: false };
+        }
+        return mission;
       });
     default:
       return state;
