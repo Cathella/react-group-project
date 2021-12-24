beb99d27310e85f8f856c5f4da2b63a363ccc169
Cathella <ncathie214@gmail.com>
2021-12-25 02:45:07 +0300

    Add profile view template

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import './Profile.css';
 
 const Profile = () => (
   <div className="container">
-    <div>
+    <div className="my-profile">
       <section>
         <h2>My Missions</h2>
         <ul>
           <li>Telstar</li>
           <li>SES</li>
           <li>AsiaSat</li>
+          <li>ABS</li>
         </ul>
       </section>
       <section>
         <h2>My Rockets</h2>
+        <ul>
+          <li>Falcon 9</li>
+          <li>Falcon Heavy</li>
+          <li>Starship</li>
+        </ul>
       </section>
     </div>
   </div>
