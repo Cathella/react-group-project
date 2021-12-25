368b839dc3b630148807a0e979b6a5e252f06ab3
Cathella <ncathie214@gmail.com>
2021-12-25 04:28:02 +0300

    Template updates

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,28 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './Profile.css';
 
-const Profile = () => (
-  <div className="container">
-    <div className="my-profile">
-      <section>
-        <h2>My Missions</h2>
-        <ul>
-          <li>Telstar</li>
-          <li>SES</li>
-          <li>AsiaSat</li>
-          <li>ABS</li>
-        </ul>
-      </section>
-      <section>
-        <h2>My Rockets</h2>
-        <ul>
-          <li>Falcon 9</li>
-          <li>Falcon Heavy</li>
-          <li>Starship</li>
-        </ul>
-      </section>
+const Profile = () => {
+  const allMissions = useSelector((state) => state.missions);
+
+  return (
+    <div className="container">
+      <div className="my-profile">
+        <section>
+          <h2>My Missions</h2>
+          <ul>
+            {/* {
+              allMissions.missions.map((mission) => {
+                if (mission.state === true) {
+                  return (
+                    <li>{mission.mission_name}</li>
+                  );
+                }
+                return false;
+              })
+            } */}
+            <li>Telstar</li>
+            <li>SES</li>
+            <li>AsiaSat</li>
+            <li>ABS</li>
+          </ul>
+        </section>
+        <section>
+          <h2>My Rockets</h2>
+          <ul>
+            <li>Falcon 9</li>
+            <li>Falcon Heavy</li>
+            <li>Starship</li>
+          </ul>
+        </section>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Profile;
