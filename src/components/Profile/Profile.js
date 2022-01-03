169019ef3c422d7a6ce67f58736e85ee591723d9
Cathella <ncathie214@gmail.com>
2022-01-03 07:26:14 +0300

    Rmove linter errors

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,35 +5,34 @@ import './Profile.css';
 const Profile = () => {
   const allMissions = useSelector((state) => state.missions);
 
+  const missionsJoined = allMissions.filter((mission) => {
+    if (mission.status === true) {
+      return mission;
+    }
+    return null;
+  });
+
   return (
     <div className="container">
       <div className="my-profile">
         <section>
           <h2>My Missions</h2>
           <ul>
-            {/* {
-              allMissions.missions.map((mission) => {
-                if (mission.state === true) {
-                  return (
-                    <li>{mission.mission_name}</li>
-                  );
-                }
-                return false;
-              })
-            } */}
-            <li>Telstar</li>
+            {/* <li>Telstar</li>
             <li>SES</li>
             <li>AsiaSat</li>
-            <li>ABS</li>
+            <li>ABS</li> */}
+            {
+              missionsJoined.map((mission) => (
+                <li key={mission.mission_id}>
+                  {mission.mission_name}
+                </li>
+              ))
+            }
           </ul>
         </section>
         <section>
           <h2>My Rockets</h2>
-          <ul>
-            <li>Falcon 9</li>
-            <li>Falcon Heavy</li>
-            <li>Starship</li>
-          </ul>
         </section>
       </div>
     </div>
